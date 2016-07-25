101473f2636804a248de52d8b48befb9ce1f8b5d
ot-maksim <ot.maksim@gmail.com>
2016-07-25 15:13:12 +0300

    GroupModificationTests class added

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by maksym on 7/25/16.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification(){
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+
+}
