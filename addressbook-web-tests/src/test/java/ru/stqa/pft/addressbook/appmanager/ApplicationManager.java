2352abb4ea1109c95d91e1534bbbc84d6110f110
ot-maksim <ot.maksim@gmail.com>
2016-08-03 11:57:31 +0300

    ability to select different drivers to run tests added; chromedriver app added
    tests can be run in Firefox, Chrome and Safari browsers

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,9 +17,20 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
   private WebDriver wd;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (browser == BrowserType.FIREFOX){
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    } else if (browser == BrowserType.SAFARI) {
+      wd = new SafariDriver();
+    }
     groupHelper = new GroupHelper(wd);
     contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
