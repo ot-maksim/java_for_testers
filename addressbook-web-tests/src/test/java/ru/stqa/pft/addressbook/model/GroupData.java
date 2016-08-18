c8fe94433c2df4fc051aff4308cd8943bfe0422c
ot-maksim <ot.maksim@gmail.com>
2016-08-18 18:28:24 +0300

    Verification of group deletion by comparing list of groups added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -5,6 +5,8 @@ public class GroupData {
   private final String header;
   private final String footer;
 
+
+
   public GroupData(String name, String header, String footer) {
     this.name = name;
     this.header = header;
@@ -21,5 +23,28 @@ public class GroupData {
 
   public String getFooter() {
     return footer;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
   }
 }
