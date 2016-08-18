31f0956b196f90f5ce7bb0466e4bb91791437768
ot-maksim <ot.maksim@gmail.com>
2016-08-18 13:03:18 +0300

    Practise with collections - Arrays and List

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,64 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.*;
+
+/**
+ * Created by maksym on 8/18/16.
+ */
+public class Collections {
+
+  public static void main(String[] args) {
+
+    //Arrays
+
+//    String[] langs = new String[4];
+//    langs[0] = "Java";
+//    langs[1] = "C#";
+//    langs[2] = "Python";
+//    langs[3] = "PHP";
+
+//    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+//    for (int i = 0; i < langs.length; i++){
+//      System.out.println("I want to learn " + langs[i]);
+//    }
+
+//    for (String i : langs) {
+//      System.out.println("I want to learn " + i);
+//    }
+
+
+    // Collections
+
+//    List<String> languages = new ArrayList<>();
+//    languages.add("Java");
+//    languages.add("C#");
+//    languages.add("Python");
+
+//    List<String> languages = Arrays.asList(langs);
+//    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+//    List languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+//    Iterator iterator = languages.iterator();
+
+//    for (String i : languages) {
+//      System.out.println("I want to learn " + i);
+//    }
+
+//    for (int i = 0; i < languages.size(); i++) {
+//      System.out.println("I want to learn " + languages.get(i));
+//    }
+
+//    while (iterator.hasNext()) {
+//      String language = (String) iterator.next();
+//      System.out.println("I want to learn " + language);
+//      if(language.equals("Java")){
+//        System.out.println(language + " is the best!");
+//      }
+//    }
+
+//    for (Object i : languages) {
+//      System.out.println("I want to learn " + i);
+//    }
+  }
+}
