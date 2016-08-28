933cf2bcce7fca5e4aea83cf0596e4a6d97414a1
ot-maksim <ot.maksim@gmail.com>
2016-08-28 18:41:32 +0300

    data generator for contacts with support of cvs, xml and  json formats added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,18 +1,33 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamOmitField;
+
 import java.io.File;
 
+@XStreamAlias("contact")
 public class ContactData {
+  @XStreamOmitField
   private int id = 0;
+  @Expose
   private String firstName;
+  @Expose
   private String lastName;
+  @Expose
   private String firstAddress;
+  @Expose
   private String homePhone;
+  @Expose
   private String mobilePhone;
+  @Expose
   private String workPhone;
   private String allPhones;
+  @Expose
   private String firstEmail;
+  @Expose
   private String secondEmail;
+  @Expose
   private String thirdEmail;
   private String allEmails;
   private String group;
