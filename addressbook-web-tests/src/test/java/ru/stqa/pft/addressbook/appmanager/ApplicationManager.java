7b92972cf51e9359b4d151b1cf15d97a2f1a2fbd
ot-maksim <ot.maksim@gmail.com>
2016-09-20 23:26:59 +0300

    platform name for macosx fixed

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -60,7 +60,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "macosx")));
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "mac")));
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
 
